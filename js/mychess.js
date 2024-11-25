d954f0b4ceb398c18add8bee9288d837dfb38918
Antonis Sidiropoulos <asidirop@gmail.com>
2024-11-25 10:53:14 +0200

    chess4

--- js/mychess.js
+++ js/mychess.js
@@ -1,49 +1,28 @@
 var me={};
 var game_status={};
+var board={};
 
-
-
-$( function() {
-    draw_empty_board(null);
-    fill_board();
-    $('#chess_reset').click(reset_board);
-
+$(function () {
+	draw_empty_board();
+	fill_board();
+	
 	$('#chess_login').click( login_to_game);
-	$('#move_div').hide(1000);
+	$('#chess_reset').click( reset_board);
 	$('#do_move').click( do_move);
-}
-);
-
-function do_move() {
-	var s = $('#the_move').val();
-	
-	var a = s.trim().split(/[ ]+/);
-	if(a.length!=4) {
-		alert('Must give 4 numbers');
-		return;
-	}
-	$.ajax({url: "chess.php/board/piece/"+a[0]+'/'+a[1], 
-			method: 'PUT',
-			dataType: "json",
-			contentType: 'application/json',
-			data: JSON.stringify( {x: a[2], y: a[3], token: me.token}),
-			success: move_result,
-			error: login_error});
-	
-}
-
-function move_result(data){
-	fill_board_by_data(data);
-}
+	$('#move_div').hide();
+	game_status_update();
+	$('#the_move_src').change( update_moves_selector);
+	$('#do_move2').click( do_move2);
+});
 
 
 function draw_empty_board(p) {
+	
 	if(p!='B') {p='W';}
 	var draw_init = {
 		'W': {i1:8,i2:0,istep:-1,j1:1,j2:9,jstep:1},
 		'B': {i1:1,i2:9,istep:1, j1:8,j2:0,jstep:-1}
 	};
-	
 	var s=draw_init[p];
 	var t='<table id="chess_table">';
 	for(var i=s.i1;i!=s.i2;i+=s.istep) {
@@ -54,32 +33,31 @@ function draw_empty_board(p) {
 		t+='</tr>';
 	}
 	t+='</table>';
-
+	
 	$('#chess_board').html(t);
+	$('.chess_square').click(click_on_piece);
 }
-
 
 function fill_board() {
-	$.ajax(
-		{url: "chess.php/board/", 
-		 success: fill_board_by_data 
-		}
-		);
+	$.ajax({url: "chess.php/board/", 
+		headers: {"X-Token": me.token},
+			//dataType: "json",
+			//contentType: 'application/json',
+			//data: JSON.stringify( {token: me.token}),
+			success: fill_board_by_data });
 }
 function reset_board() {
-	$.ajax(
-		{url: "chess.php/board/", 
-         method: 'post',
-		 success: fill_board_by_data 
-		}
-		);
+	$.ajax({url: "chess.php/board/", headers: {"X-Token": me.token}, method: 'POST',  success: fill_board_by_data });
+	$('#move_div').hide();
+	$('#game_initializer').show(2000);
 }
 function fill_board_by_data(data) {
+	board=data;
 	for(var i=0;i<data.length;i++) {
 		var o = data[i];
 		var id = '#square_'+ o.x +'_' + o.y;
 		var c = (o.piece!=null)?o.piece_color + o.piece:'';
-        var im = (o.piece!=null)?'<img class="piece" src="images/'+c+'.png">':'';
+		var im = (o.piece!=null)?'<img class="piece" src="images/'+c+'.png">':'';
 		$(id).addClass(o.b_color+'_square').html(im);
 		
 	}
@@ -97,6 +75,7 @@ function login_to_game() {
 	$.ajax({url: "chess.php/players/"+p_color, 
 			method: 'PUT',
 			dataType: "json",
+			headers: {"X-Token": me.token},
 			contentType: 'application/json',
 			data: JSON.stringify( {username: $('#username').val(), piece_color: p_color}),
 			success: login_result,
@@ -110,34 +89,100 @@ function login_result(data) {
 	game_status_update();
 }
 
-function update_info(){
-	$('#game_info').html("I am Player: "+me.piece_color+", my name is "+me.username +'<br>Token='+me.token+'<br>Game state: '+game_status.status+', '+ game_status.p_turn+' must play now.');	
-}
-
 function login_error(data,y,z,c) {
 	var x = data.responseJSON;
 	alert(x.errormesg);
 }
 
 function game_status_update() {
-	$.ajax({url: "chess.php/status/", success: update_status });
+	$.ajax({url: "chess.php/status/", success: update_status,headers: {"X-Token": me.token} });
 }
 
 function update_status(data) {
-	if (game_status.p_turn==null ||  data[0].p_turn != game_status.p_turn ||  data[0].status != game_status.status) {
-		fill_board();
-	}
+	var game_stat_old = game_status;
 	game_status=data[0];
 	update_info();
-	 if(game_status.p_turn==me.piece_color &&  me.piece_color!=null) {
+	if(game_status.p_turn==me.piece_color &&  me.piece_color!=null) {
 		x=0;
 		// do play
+		if(game_stat_old.p_turn!=me.piece_color) {
+			fill_board();
+		}
 		$('#move_div').show(1000);
 		setTimeout(function() { game_status_update();}, 15000);
 	} else {
 		// must wait for something
 		$('#move_div').hide(1000);
 		setTimeout(function() { game_status_update();}, 4000);
-	} 
+	}
  	
+}
+
+function update_info(){
+	$('#game_info').html("I am Player: "+me.piece_color+", my name is "+me.username +'<br>Token='+me.token+'<br>Game state: '+game_status.status+', '+ game_status.p_turn+' must play now.');
+	
+	
+}
+
+function do_move() {
+	var s = $('#the_move').val();
+	
+	var a = s.trim().split(/[ ]+/);
+	if(a.length!=4) {
+		alert('Must give 4 numbers');
+		return;
+	}
+	$.ajax({url: "chess.php/board/piece/"+a[0]+'/'+a[1], 
+			method: 'PUT',
+			dataType: "json",
+			contentType: 'application/json',
+			data: JSON.stringify( {x: a[2], y: a[3]}),
+			headers: {"X-Token": me.token},
+			success: move_result,
+			error: login_error});
+	
+}
+
+function move_result(data){
+	fill_board_by_data(data);
+	$('#move_div').hide(1000);
+}
+
+function update_moves_selector() {
+	$('.chess_square').removeClass('pmove').removeClass('tomove');
+	var s = $('#the_move_src').val();
+	var a = s.trim().split(/[ ]+/);
+	$('#the_move_dest').html('');
+	if(a.length!=2) {
+		return;
+	}
+	var id = '#square_'+ a[0]+'_'+a[1];
+	$(id).addClass('tomove');
+	for(i=0;i<board.length;i++) {
+		if(board[i].x==a[0] && board[i].y==a[1]) {
+			for(m=0;m<board[i].moves.length;m++) {
+				$('#the_move_dest').append('<option value="'+board[i].moves[m].x+' '+board[i].moves[m].y+'">'+board[i].moves[m].x+' '+board[i].moves[m].y+'</option>');
+				var id = '#square_'+ board[i].moves[m].x +'_' + board[i].moves[m].y;
+				$(id).addClass('pmove');
+			}
+			
+		}
+	}
+}
+
+function do_move2() {
+	$('#the_move').val($('#the_move_src').val() +' ' + $('#the_move_dest').val());
+	do_move();
+	$('.chess_square').removeClass('pmove').removeClass('tomove');
+}
+
+function click_on_piece(e) {
+	var o=e.target;
+	if(o.tagName!='TD') {o=o.parentNode;}
+	if(o.tagName!='TD') {return;}
+	
+	var id=o.id;
+	var a=id.split(/_/);
+	$('#the_move_src').val(a[1]+' ' +a[2]);
+	update_moves_selector();
 }
--- lib/board.php
+++ lib/board.php
@@ -1,25 +1,8 @@
 <?php
-function show_board() {
-    global $mysqli;
-	
-	$sql = 'select * from board';
-	$st = $mysqli->prepare($sql);
-	$st->execute();
-	$res = $st->get_result();
-	header('Content-type: application/json');
-	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
-}
 
-function reset_board() {
-	global $mysqli;
-	
-	$sql = 'call clean_board()';
-	$mysqli->query($sql);
-	show_board();
-}
-
-
+  
 function move_piece($x,$y,$x2,$y2,$token) {
+	
 	if($token==null || $token=='') {
 		header("HTTP/1.1 400 Bad Request");
 		print json_encode(['errormesg'=>"token is not set."]);
@@ -43,27 +26,25 @@ function move_piece($x,$y,$x2,$y2,$token) {
 		print json_encode(['errormesg'=>"It is not your turn."]);
 		exit;
 	}
-//	$orig_board=read_board();
-// $board=convert_board($orig_board);
-//	$n = add_valid_moves_to_piece($board,$color,$x,$y);
+	$orig_board=read_board();
+	$board=convert_board($orig_board);
+	$n = add_valid_moves_to_piece($board,$color,$x,$y);
 	
-//	if($n==0) {
-//		header("HTTP/1.1 400 Bad Request");
-//		print json_encode(['errormesg'=>"This piece cannot move."]);
-//		exit;
-//	}
-//	foreach($board[$x][$y]['moves'] as $i=>$move) {
-//		if($x2==$move['x'] && $y2==$move['y']) {
+	if($n==0) {
+		header("HTTP/1.1 400 Bad Request");
+		print json_encode(['errormesg'=>"This piece cannot move."]);
+		exit;
+	}
+	foreach($board[$x][$y]['moves'] as $i=>$move) {
+		if($x2==$move['x'] && $y2==$move['y']) {
 			do_move($x,$y,$x2,$y2);
 			exit;
-//		}
-//	}
+		}
+	}
 	header("HTTP/1.1 400 Bad Request");
 	print json_encode(['errormesg'=>"This move is illegal."]);
 	exit;
-
 }
-
 function do_move($x,$y,$x2,$y2) {
 	global $mysqli;
 	$sql = 'call `move_piece`(?,?,?,?);';
@@ -71,9 +52,9 @@ function do_move($x,$y,$x2,$y2) {
 	$st->bind_param('iiii',$x,$y,$x2,$y2 );
 	$st->execute();
 
-	show_board();
+	header('Content-type: application/json');
+	print json_encode(read_board(), JSON_PRETTY_PRINT);
 }
-
 function show_piece($x,$y) {
 	global $mysqli;
 	
@@ -85,6 +66,141 @@ function show_piece($x,$y) {
 	header('Content-type: application/json');
 	print json_encode($res->fetch_all(MYSQLI_ASSOC), JSON_PRETTY_PRINT);
 }
+function show_board($input) {
+	global $mysqli;
+	
+	$b=current_color($input['token']);
+	if($b) {
+		show_board_by_player($b);
+	} else {
+		header('Content-type: application/json');
+		print json_encode(read_board(), JSON_PRETTY_PRINT);
+	}
+}
+function show_board_by_player($b) {
 
+	global $mysqli;
+
+	$orig_board=read_board();
+	$board=convert_board($orig_board);
+	$status = read_status();
+	if($status['status']=='started' && $status['p_turn']==$b && $b!=null) {
+		// It my turn !!!!
+		$n = add_valid_moves_to_board($board,$b);
+		
+		// Εάν n==0, τότε έχασα !!!!!
+		// Θα πρέπει να ενημερωθεί το game_status.
+	}
+	header('Content-type: application/json');
+	print json_encode($orig_board, JSON_PRETTY_PRINT);
+}
+
+
+function add_valid_moves_to_board(&$board,$b) {
+	$number_of_moves=0;
+	
+	for($x=1;$x<9;$x++) {
+		for($y=1;$y<9;$y++) {
+			$number_of_moves+=add_valid_moves_to_piece($board,$b,$x,$y);
+		}
+	}
+	return($number_of_moves);
+}
+
+
+
+function add_valid_moves_to_piece(&$board,$b,$x,$y) {
+	$number_of_moves=0;
+	if($board[$x][$y]['piece_color']==$b) {
+		switch($board[$x][$y]['piece']){
+			case 'P': $number_of_moves+=pawn_moves($board,$b,$x,$y);break;
+			case 'K': $number_of_moves+=king_moves($board,$b,$x,$y);break;
+			case 'Q': $number_of_moves+=queen_moves($board,$b,$x,$y);break;
+			case 'R': $number_of_moves+=rook_moves($board,$b,$x,$y);break;
+			case 'N': $number_of_moves+=knight_moves($board,$b,$x,$y);break;
+			case 'B': $number_of_moves+=bishop_moves($board,$b,$x,$y);break;
+		}
+	} 
+	return($number_of_moves);
+}
+
+
+function king_moves(&$board,$b,$x,$y) {
+	return(0);
+}
+function queen_moves(&$board,$b,$x,$y) {return(0);}
+function rook_moves(&$board,$b,$x,$y) {return(0);}
+
+function bishop_moves(&$board,$b,$x,$y) {return(0);}
+
+function knight_moves(&$board,$b,$x,$y) {
+	$m = [
+		[2,1],
+		[1,2],
+		[2,-1],
+		[1,-2],
+		[-2,1],
+		[-1,2],
+		[-2,-1],
+		[-1,-2],
+	];
+	$moves=[];
+	foreach($m as $k=>$t) {
+		$x2=$x+$t[0];
+		$y2=$y+$t[1];
+		if( $x2>=1 && $x2<=8 && $y2>=1 && $y2<=8 &&
+			$board[$x2][$y2]['piece_color'] !=$b ) {
+			// Αν ο προορισμός είναι εντός σκακιέρας και δεν υπάρχει δικό μου πιόνι
+			$move=['x'=>$x2, 'y'=>$y2];
+			$moves[]=$move;
+		}
+	}
+	$board[$x][$y]['moves'] = $moves;
+	return(sizeof($moves));
+}
+
+function pawn_moves(&$board,$b,$x,$y) {
+	
+	$direction=($b=='W')?1:-1;
+	$start_row = ($b=='W')?2:7;
+	$moves=[];
+	
+	if($board[$x][$y+$direction]['piece_color']==null) {
+		$move=['x'=>$x, 'y'=>$y+$direction];
+		$moves[]=$move;
+		if($y==$start_row && $board[$x][$y+2*$direction]['piece_color']==null) {
+			$move=['x'=>$x, 'y'=>$y+2*$direction];
+			$moves[]=$move;
+		}
+	}
+	$board[$x][$y]['moves'] = $moves;
+	return(sizeof($moves));
+	
+}
+
+
+function convert_board(&$orig_board) {
+	$board=[];
+	foreach($orig_board as $i=>&$row) {
+		$board[$row['x']][$row['y']] = &$row;
+	} 
+	return($board);
+}
+
+function read_board() {
+	global $mysqli;
+	$sql = 'select * from board';
+	$st = $mysqli->prepare($sql);
+	$st->execute();
+	$res = $st->get_result();
+	return($res->fetch_all(MYSQLI_ASSOC));
+}
+function reset_board() {
+	global $mysqli;
+	
+	$sql = 'call clean_board()';
+	$mysqli->query($sql);
+	show_board();
+}
 
 ?>
